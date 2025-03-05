49785f0ac19c951226bcdeb624278254bac40e83
Serveriii <sjokelainen@outlook.com>
2025-03-05 17:25:30 +0200

    Cors fixd

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "http://localhost:3000", "http://localhost:5173", "https://serveriii.github.io/Work-Hours-Frontend/"
+    origins "http://localhost:3001", "http://localhost:3000", "http://localhost:5173", "https://serveriii.github.io"
 
     resource "*",
       headers: :any,
